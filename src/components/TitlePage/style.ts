2bdbb6a6fa54893e3c9655544787e30a4ca0ece3
Derick-Almeida <d.a.s2000silva14@gmail.com>
2023-09-15 08:02:21 -0300

    feat: page title

---
+++ src/components/TitlePage/style.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const h2 = styled.h2`
+  text-transform: uppercase;
+  font-family: var(--monoton);
+  font-size: 5rem;
+  font-weight: 400;
+
+  color: var(--aqua);
+  opacity: 0.4;
+`;
