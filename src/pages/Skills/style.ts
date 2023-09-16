b8c2302951f7deda7f23742da622f7e2a8aa495d
Derick-Almeida <d.a.s2000silva14@gmail.com>
2023-09-16 16:31:37 -0300

    feat: skill page

---
+++ src/pages/Skills/style.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const container = styled.div`
+  padding: 0 10% 5rem 10%;
+`;
+
+export const box = styled.div`
+  display: grid;
+  grid-template-areas:
+    ". . image . ."
+    ". . image . ."
+    ". . image . ."
+    ". . image . ."
+    ". . . . .";
+  grid-gap: 2rem;
+`;
+
+export const img = styled.img`
+  grid-area: image;
+  width: 100%;
+`;
