6340c2bb9377e521eb302faea6d8a29415da989f
Derick-Almeida <d.a.s2000silva14@gmail.com>
2023-09-16 22:05:04 -0300

    feat: projects page

---
+++ src/pages/Projects/style.ts
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const container = styled.div`
+  padding: 0 10% 10rem 10%;
+`;
+
+export const box = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: center;
+  gap: 2rem;
+  margin-top: 5rem;
+`;
+
+export const div = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 48%;
+`;
+
+export const img = styled.img`
+  width: 60%;
+`;
